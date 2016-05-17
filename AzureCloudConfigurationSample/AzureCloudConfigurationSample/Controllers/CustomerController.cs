8f7e008c4bd2764d679872020a4460ae23762c58
Chris Dusyk <chris.dusyk@gmail.com>
2016-05-17 11:51:44 -0600

    Added first controller and view to sample

---
+++ AzureCloudConfigurationSample/AzureCloudConfigurationSample/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AzureCloudConfigurationSample.Models.Entities;
+
+namespace AzureCloudConfigurationSample.Controllers
+{
+	public class CustomerController : Controller
+	{
+		private MariaDBContext db = new MariaDBContext();
+
+		// GET: Customer
+		public ActionResult Index()
+		{
+			var list = db.Customers.ToList();
+
+			return View(list);
+		}
+	}
+}

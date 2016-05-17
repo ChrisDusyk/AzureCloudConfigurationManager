97eb2ebe016b012703df6159f5e5fcaae3187134
Chris Dusyk <chris.dusyk@gmail.com>
2016-05-17 13:32:28 -0600

    Added controller with pictures for tutorial

--- AzureCloudConfigurationSample/AzureCloudConfigurationSample/Controllers/CustomerController.cs
+++ AzureCloudConfigurationSample/AzureCloudConfigurationSample/Controllers/CustomerController.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AzureCloudConfigurationSample.Models.Entities;
 
 namespace AzureCloudConfigurationSample.Controllers
 {
-	public class CustomerController : Controller
-	{
-		private MariaDBContext db = new MariaDBContext();
-
-		// GET: Customer
-		public ActionResult Index()
-		{
-			var list = db.Customers.ToList();
-
-			return View(list);
-		}
-	}
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
 }

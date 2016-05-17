39677ca62233548222cd0dc54bb638bc7c7c8b1c
Chris Dusyk <chris.dusyk@gmail.com>
2016-05-17 11:15:16 -0600

    Add data classes for EF

---
+++ AzureCloudConfigurationSample/AzureCloudConfigurationSample/Models/Entities/MariaDBContext.cs
@@ -0,0 +1,28 @@
+namespace AzureCloudConfigurationSample.Models.Entities
+{
+	using System.Data.Entity;
+
+	[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+	public partial class MariaDBContext : DbContext
+	{
+		public MariaDBContext() : base("name=MariaDBContext")
+		{
+			Database.SetInitializer<MariaDBContext>(new MariaDbInitializer());
+		}
+
+		public virtual DbSet<Customer> Customers { get; set; }
+
+		public virtual DbSet<Order> Orders { get; set; }
+
+		public virtual DbSet<Product> Products { get; set; }
+
+		public virtual DbSet<OrderProduct> OrderProducts { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Customer>()
+				.Property(e => e.CustomerName)
+				.IsUnicode(false);
+		}
+	}
+}
---
+++ AzureCloudConfigurationSample/AzureCloudConfigurationSample/Models/Entities/MariaDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace AzureCloudConfigurationSample.Models.Entities
+{
+	public class MariaDbInitializer : CreateDatabaseIfNotExists<MariaDBContext>
+	{
+		protected override void Seed(MariaDBContext context)
+		{
+			base.Seed(context);
+		}
+	}
+}
